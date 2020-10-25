f397cea804ee7113f6fef17409e34da496d6033b
Lawblaze <56140026+lawsonDaniel@users.noreply.github.com>
2020-10-24 19:18:46 -0700

    Add files via upload

---
+++ atomating.py
@@ -0,0 +1,107 @@
+import pyautogui
+print(pyautogui.position())
+
+print(pyautogui.position())
+pyautogui.click(508,224)
+distance=200
+
+while distance>0:
+      print(distance,0)
+      pyautogui.dragRel(-distance,0,duration=1)
+      distance = distance-25
+      print(0,distance)
+      pyautogui.dragRel(0,distance,duration=1)
+     
+
+      pyautogui.dragRel(distance,0,duration=1)
+      distance= distance-25
+      print(0,-distance)
+      pyautogui.dragRel(0,-distance,duration=1)
+
+      pyautogui.dragRel(-distance,0,duration=1)
+      distance = distance-25
+      print(0,distance)
+      pyautogui.dragRel(0,distance,duration=1)
+     
+
+      pyautogui.dragRel(distance,0,duration=1)
+      distance= distance-25
+      print(0,-distance)
+      pyautogui.dragRel(0,-distance,duration=1)
+
+      pyautogui.dragRel(-distance,0,duration=1)
+      distance = distance-25
+      print(0,distance)
+      pyautogui.dragRel(0,distance,duration=1)
+     
+
+      pyautogui.dragRel(distance,0,duration=1)
+      distance= distance-25
+      print(0,-distance)
+      pyautogui.dragRel(0,-distance,duration=1)
+
+      pyautogui.dragRel(-distance,0,duration=1)
+      distance = distance-25
+      print(0,distance)
+      pyautogui.dragRel(0,distance,duration=1)
+     
+
+      pyautogui.dragRel(distance,0,duration=1)
+      distance= distance-25
+      print(0,-distance)
+      pyautogui.dragRel(0,-distance,duration=1)
+
+# pyautogui.click(243,100)      
+# pyautogui.click(708,224)
+# distance=200
+# pyautogui.displayMousePosition()
+# while distance>0:
+#       print(distance,0)
+#       pyautogui.dragRel(distance,0,duration=1)
+#       distance = distance-25
+#       print(0,distance)
+#       pyautogui.dragRel(0,-distance,duration=1)
+     
+
+#       pyautogui.dragRel(-distance,0,duration=1)
+#       distance= distance-25
+#       print(0,-distance)
+#       pyautogui.dragRel(0,distance,duration=1)
+
+#       pyautogui.dragRel(-distance,0,duration=1)
+#       distance = distance-25
+#       print(0,distance)
+#       pyautogui.dragRel(0,-distance,duration=1)
+     
+
+#       pyautogui.dragRel(-distance,0,duration=1)
+#       distance= distance-25
+#       print(0,-distance)
+#       pyautogui.dragRel(0,distance,duration=1)
+
+#       pyautogui.dragRel(distance,0,duration=1)
+#       distance = distance-25
+#       print(0,distance)
+#       pyautogui.dragRel(0,-distance,duration=1)
+     
+
+#       pyautogui.dragRel(-distance,0,duration=1)
+#       distance= distance-25
+#       print(0,-distance)
+#       pyautogui.dragRel(0,distance,duration=1)
+
+#       pyautogui.dragRel(distance,0,duration=1)
+#       distance = distance-25
+#       print(0,distance)
+#       pyautogui.dragRel(0,-distance,duration=1)
+     
+
+#       pyautogui.dragRel(-distance,0,duration=1)
+#       distance= distance-25
+#       print(0,-distance)
+#       pyautogui.dragRel(0,distance,duration=1)
+# 
+
+   
+
+
---
+++ tolontolon.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+print(pyautogui.position())
+pyautogui.moveTo(27,54)
+pyautogui.click(27,54,clicks=2)
+pyautogui.moveTo(27,92)
+pyautogui.click(27,92,clicks=2)
+pyautogui.moveTo(564,404)
+pyautogui.click(564,404,clicks=2)
