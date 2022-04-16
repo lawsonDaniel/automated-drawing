f40c8b72f6ef2cc0d6017f4a410c142c978a7bac
Lawblaze <lawblaze4@gmail.com>
2022-04-17 00:03:44 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-
-print(pyautogui.position())
-pyautogui.moveTo(250,323,duration=5)
-pyautogui.click()
-pyautogui.dragRel(200,0,duration=2)
-pyautogui.moveTo(296,265,duration=2)
-pyautogui.click()
-pyautogui.dragRel(0,200,duration=2)
